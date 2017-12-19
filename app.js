989ebb38ff5848bbe22f9a9a4d913a1bd2212b38
matt-winzer <matt.winzer@gmail.com>
2017-12-19 10:35:16 -0700

    setup for GET and POST

--- app.js
+++ app.js
@@ -2,28 +2,5 @@
   // Request to get information from an API
   // Different API endpoints => different data returned
 
-var baseUrl = 'https://drageons.herokuapp.com/'
+var baseUrl = 'https://swapi.co/api/'
 
-// Fetch: First argument is the url we want data from
-
-// Request it
-// Get it
-// Translate it
-// Work with it
-
-fetch(baseUrl + 'character/1')
-  .then(response => {
-    return response.json()
-  })
-  .then(response => {
-    var name = response[0].name
-    // Do SOMETHING with the response data
-    // Often DOM manipulation
-    console.log(response)
-    console.log(name)
-    console.log(response[0].features[0].class)
-  })
-  .catch(error => {
-    console.log(error)
-    // DOM manipulation to express that there was an error
-  })
--- fetch.md
+++ fetch.md
@@ -2,6 +2,50 @@
 
 ## Objectives
 
+* Use fetch to GET data from an API
+* Use fetch to POST data to an api
+* Explain the use case for fetch
+
+## Notes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Objectives
+
 * Describe the use case for fetch
 * Identify alternatives to fetch
   - jQuery
